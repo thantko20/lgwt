b1fdd1467fe6eb0ddfda867c43d059fd6e0e0644
Thant Ko Zaw <thantmkou10@gmail.com>
2024-06-18 22:59:37 +0630

    pointers finished

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,23 +14,24 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("works for rectangles", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 10.0}
-		got := rectangle.Area()
-		want := 50.00
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 5.0, Height: 10.0}, want: 50.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-	t.Run("works for circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
 }
