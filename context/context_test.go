b4cf4bb5c855584bc225ffaef0670037f2db44b8
Thant Ko Zaw <thantmkou10@gmail.com>
2024-06-25 22:13:56 +0630

    init context lesson

---
+++ context/context_test.go
@@ -0,0 +1,36 @@
+package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	server := Server(&SpyStore{response: data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	server.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got %s want %s", response.Body.String(), data)
+	}
+}
