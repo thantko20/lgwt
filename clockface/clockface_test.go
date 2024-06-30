efc0494d69bd8620ef99f14c26a7a4779c656827
Thant Ko Zaw <thantmkou10@gmail.com>
2024-07-01 01:11:08 +0630

    start maths lesson

---
+++ clockface/clockface_test.go
@@ -0,0 +1,27 @@
+package clockface_test
+
+import (
+	"lgwt/clockface"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(2000, time.January, 1, 0, 0, 30, 0, time.UTC)
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
