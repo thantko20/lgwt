b9683c9beadd1afa5c8437d7501c4d6394588bb5
Thant Ko Zaw <thantmkou10@gmail.com>
2024-07-06 22:02:19 +0630

    array slices generics wip

--- arrays-slices/arrays-slices.go
+++ arrays-slices/arrays-slices.go
@@ -1,22 +1,44 @@
 package main
 
 func Sum(numbers []int) (sum int) {
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	return Reduce(numbers, func(acc int, curr int) int {
+		return acc + curr
+	}, 0)
 }
 
 func SumAllTails(numberSlices ...[]int) []int {
 	var sums []int
-	for _, numbers := range numberSlices {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+	return Reduce(numberSlices, func(acc []int, curr []int) []int {
+		if len(curr) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := curr[1:]
+			return append(acc, Sum(tail))
+		}
+	}, sums)
+}
+
+func Reduce[T any, P any](collection []T, fn func(acc P, curr T) P, initial P) P {
+	for _, item := range collection {
+		initial = fn(initial, item)
+	}
+	return initial
+}
+
+type Transaction struct {
+	From, To string
+	Sum      float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
 		}
 	}
-
-	return sums
+	return balance
 }
--- arrays-slices/arrays-slices_test.go
+++ arrays-slices/arrays-slices_test.go
@@ -22,6 +22,7 @@ func TestSum(t *testing.T) {
 func TestSumAllTails(t *testing.T) {
 
 	assert := func(t testing.TB, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
@@ -40,4 +41,53 @@ func TestSumAllTails(t *testing.T) {
 
 		assert(t, got, want)
 	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of integers", func(t *testing.T) {
+		multiply := func(a, b int) int {
+			return a * b
+		}
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenation of strings", func(t *testing.T) {
+		concat := func(a, b string) string {
+			return a + b
+		}
+		AssertEqual(t, Reduce([]string{"Hello", ", ", "World!"}, concat, ""), "Hello, World!")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v want %+v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %+v", want)
+	}
 }
